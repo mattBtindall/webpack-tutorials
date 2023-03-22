019fce2e45158e4a2b9363eddde9c6c3238ff7c4
mattBtindall <matthewbtindall@gmail.com>
2023-03-22 12:02:37 +0000

    Output Management

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import _ from 'lodash';
-import './style.css';
+import printMe from './print.js';
 
 function component() {
-  const element = document.createElement('div');
+    const element = document.createElement('div');
+    const btn = document.createElement('button');
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
 
-  return element;
+    element.appendChild(btn);
+
+    return element;
 }
 
 document.body.appendChild(component());
